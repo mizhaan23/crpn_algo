e3321e955331f44cf2698d1f90a939a5039b43c3
mizhaan23 <mizhaan.23@gmail.com>
2024-02-25 03:47:01 +0530

    Added hyperparameter dict for each env.

--- aistats_paper/test_runner.py
+++ aistats_paper/test_runner.py
@@ -1,30 +1,60 @@
 import subprocess
 import time
 
+config = {
+    "CartPole-v1": {
+        'alpha': '1000',
+        'num-updates': '1000',
+        'max-timesteps': '500',  # horizon
+        'batch-size': '50',
+        'hidden-sizes': '()',  # linear features ONLY
+    },
+
+    "Reacher-v4": {
+        'alpha': '10000',
+        'num-updates': '1000',
+        'max-timesteps': '50',  # horizon
+        'batch-size': '100',
+        'hidden-sizes': '(32, 32)',
+    },
+
+    "Humanoid-v4": {
+        'alpha': '10000',
+        'num-updates': '1000',
+        'max-timesteps': '1000',  # horizon
+        'batch-size': '100',
+        'hidden-sizes': '(64, 64)',
+    }
+}
+
 if __name__ == "__main__":
 
     NUM_SIMULATIONS = 10
-    ALGO = 'sgd'  # {sgd, sgd2, acrpn}
+    ENV = "CartPole-v1"
+    env_config = config[ENV]
+    ALGO = 'acrpn'  # {sgd, acrpn}
 
     t_ = time.time()
 
     for s in range(NUM_SIMULATIONS):
+
         print(f"SIMULATION NO : {s + 1}", end="\n")
 
         command = [
             'python',
-            f'mujoco_experiments/{ALGO}.py',
+            f'aistats_paper/mujoco_experiments/{ALGO}.py',
             '--exp-name', f'{ALGO.upper()}{s + 1}_FINAL',
             '--env-seed', '-1',
             '--save', 'True',
             '--track', 'False',
-            '--alpha', '100_000',
+            '--alpha', env_config['alpha'],
             '--normalize-returns', 'True',
 
-            '--num-updates', '1000',
-            '--max-timesteps', '1000',
-            '--gym-id', 'Humanoid-v4',
-            '--hidden-sizes', '(64, 64)',
+            '--num-updates', env_config['num-updates'],
+            '--max-timesteps', env_config['max-timesteps'],
+            '--gym-id', ENV,
+            '--hidden-sizes', env_config['hidden-sizes'],
+            '--batch-size', env_config['batch-size']
         ]
 
         subprocess.run(command)
