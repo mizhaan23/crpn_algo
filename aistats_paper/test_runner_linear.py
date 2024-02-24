e35e9461c473c9d57a07b18e0d438ee0522f1198
mizhaan23 <mizhaan.23@gmail.com>
2024-02-25 03:47:19 +0530

    Added hyperparameter dict for each env for linear experiments.

---
+++ aistats_paper/test_runner_linear.py
@@ -0,0 +1,44 @@
+import subprocess
+import time
+
+config = {
+    "CartPole-v1": {
+        'alpha': '1000',
+        'num-updates': '1000',
+        'max-timesteps': '500',  # horizon
+        'batch-size': '50',
+    }
+}
+
+if __name__ == "__main__":
+
+    NUM_SIMULATIONS = 10
+    ENV = "CartPole-v1"
+    env_config = config[ENV]
+    ALGO = 'sgd'  # {sgd, crpn}
+
+    t_ = time.time()
+
+    for s in range(NUM_SIMULATIONS):
+
+        print(f"SIMULATION NO : {s + 1}", end="\n")
+
+        command = [
+            'python',
+            f'aistats_paper/linear/{ALGO}.py',
+            '--exp-name', f'{ALGO.upper()}{s + 1}_LINEARFINAL',
+            '--env-seed', '-1',
+            '--save', 'True',
+            '--track', 'False',
+            '--alpha', env_config['alpha'],
+            '--normalize-returns', 'True',
+
+            '--num-updates', env_config['num-updates'],
+            '--max-timesteps', env_config['max-timesteps'],
+            '--gym-id', ENV,
+            '--batch-size', env_config['batch-size']
+        ]
+
+        subprocess.run(command)
+
+    print("Simulations complete. Time taken:", round(time.time() - t_, 3))
