6500de61e3fd28de5bd61061f07075c055ac0a9f
mizhaan23 <mizhaan.23@gmail.com>
2024-03-07 16:09:28 +0530

    Rename files.

--- linear/crpn.py
+++ linear/crpn.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from distutils.util import strtobool
 import shutil
 
-from algo_linear.crpn_linear import LinearPolyCRPN
+from algo._crpn import LinearPolyCRPN
 import wandb
 
 
--- linear/reinforce.py
+++ linear/reinforce.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from distutils.util import strtobool
 import shutil
 
-from algo_linear.sgd_linear import LinearPolySGD
+from algo._reinforce import LinearPolySGD
 import wandb
 
 
