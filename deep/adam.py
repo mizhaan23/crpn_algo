9fa9bc3dfa96eab596c9b42e91247cb44c1d6162
mizhaan23 <mizhaan.23@gmail.com>
2024-03-07 16:43:37 +0530

    Minor changes

--- deep/adam.py
+++ deep/adam.py
@@ -15,18 +15,6 @@ from policies import GaussianMLPPolicy, CategoricalMLPPolicy, CategoricalLinearP
 from utils import simulate_trajectories, discount_cumsum
 
 
-def calculate_time(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Time taken to execute {func.__name__}: {elapsed_time} seconds")
-        return result
-
-    return wrapper
-
-
 def parse_args():
     parser = argparse.ArgumentParser()
 
