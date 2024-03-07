f0c19507aec8dc0982fe8e5aa0a8794955ba4568
mizhaan23 <mizhaan.23@gmail.com>
2024-03-07 16:44:31 +0530

    Minor changes

--- deep/utils/_utils.py
+++ deep/utils/_utils.py
@@ -1,5 +1,18 @@
+import time
 import numpy as np
 import torch
+
+
+def calculate_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Time taken to execute {func.__name__}: {elapsed_time} seconds")
+        return result
+
+    return wrapper
 
 
 def simulate_trajectories(envs, policy, horizon, device):
