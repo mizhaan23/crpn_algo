73e1042884416c33974fbc99fa9477ce317473c1
Mizhaan Maniyar <104845333+mizhaan23@users.noreply.github.com>
2024-03-18 15:56:51 +0530

    Revert "Removed unnecessary files."
    This reverts commit f8c5ffe34b80f393fc196a673616dbf177df5a7d.

--- crpn_algo/deep/algo/_acrpn.py
+++ crpn_algo/deep/algo/_acrpn.py
@@ -5,7 +5,18 @@ from torch.optim.optimizer import Optimizer
 from typing import Dict, List, Tuple, Optional, Callable
 import time
 from copy import deepcopy
-from deep.utils import calculate_time
+
+
+def calculate_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Time taken to execute {func.__name__}: {elapsed_time} seconds")
+        return result
+
+    return wrapper
 
 
 class ACRPN(Optimizer):
